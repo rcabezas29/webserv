e03271df4c5889a2bbaea1904b78071e7971210a
David Puente rodriguez <dpuente-@c1r16s6.42madrid.com>
2022-07-07 19:24:47 +0200

    Utils.h y main.cpp para hacer pruebas

---
+++ libraries/utils.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 18:54:09 by dpuente-          #+#    #+#             */
+/*   Updated: 2022/07/07 19:21:18 by dpuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# pragma once
+# ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <cstring>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+
+
+# endif
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 18:54:35 by dpuente-          #+#    #+#             */
+/*   Updated: 2022/07/07 19:22:41 by dpuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libraries/utils.hpp"
+
+/* # define AF_INET=
+# define SOCK_STREAM */
+
+int main(void)
+{
+    int sever_fd = socket(/* domain */, /* __cpp_return_type_deduction */);
+    return(0);
+}   
