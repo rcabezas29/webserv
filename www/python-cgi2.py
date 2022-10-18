690d2d3337e7185979e3529c7aaf7abcecb3f844
dpuente- <dpuente-@example.com>
2022-10-18 20:17:30 +0200

    python cgi cosas

---
+++ www/python-cgi2.py
@@ -0,0 +1,37 @@
+import sys
+
+headers = \
+"""
+X-Powered-By: Python/3.9
+Content-type: text/html; charset=UTF-8
+"""
+
+response = \
+"""
+<html>
+        <head>
+                <title>Python CGI Test</title>
+        </head>
+        <body>
+                <p>
+					Hello Python CGI
+				</p> 
+        </body>
+</html>
+
+"""
+
+def text_file(file):
+	with open(file, "r+") as r:
+		content = r.readlines()
+	return("".join(content))
+
+
+if __name__ == "__main__":
+	n = len(sys.argv)
+	if n > 1:
+		print("ERROR: Bad arguments")
+	else:
+		# file = sys.argv[1]
+		# print(f"{headers}\n{response1}{text_file(file)}{response2}")
+		 print(f"{headers}\n{response}")
